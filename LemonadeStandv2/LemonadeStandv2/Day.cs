bbaf2cc30ddff12489dff3f13b399fa8db986b78
gregsmith78 <ga.smithr78@gmail.com>
2016-04-11 12:01:24 -0500

    added complexity to buying function

--- LemonadeStandv2/LemonadeStandv2/Day.cs
+++ LemonadeStandv2/LemonadeStandv2/Day.cs
@@ -11,25 +11,33 @@ namespace LemonadeStandv2
        
         
         public int numberOfCustomers;
-       // public Store store;
-        //public Weather weather;
-
-
+    
         public Day()
         {
-
-           // store = new Store();
-            //weather = new Weather();
-           
-           
+        
         }
+
         public double CustomersBuying(Player player, Store store)
         {
+            Random rand = new Random();
+           
+
             for (int potentialCust = 0; potentialCust < numberOfCustomers; potentialCust++)
             {
+                int buyFactor = rand.Next(1, 10);
                 if (store.chargePerLemonade > 0 && store.chargePerLemonade < .50)
                 {
+                    if (buyFactor < 6)
+                    { 
+
                     player.wallet += store.chargePerLemonade;
+                    Console.WriteLine("bought lemonade.");
+                    }
+                    else
+                    {
+                       
+                        Console.WriteLine("did not buy lemonade.");
+                    }
                 }
             }
             return player.wallet;
